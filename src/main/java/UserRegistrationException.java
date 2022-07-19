72f4624c460cda623b86a8ad2247e0f9db227504
Your Name <rahul4girgal@gmail.com>
2022-07-19 13:34:56 +0530

    Added :Refactore the code to throw custom exception in case invalid userdetails

---
+++ src/main/java/UserRegistrationException.java
@@ -0,0 +1,7 @@
+public class UserRegistrationException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
